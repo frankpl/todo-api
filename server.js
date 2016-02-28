fe8ba473af3682e239e020cd8a943e4d4faeb4ff
frankpl <frankpl@outlook.com>
2016-02-28 08:07:09 -0500

    add POST api and body-parser package

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Got to get the milk',
-    completed: false
-}, {
-    id: 2,
-    description: 'Don\'t forget the bread',
-    completed: false
-}, {
-    id: 3,
-    description: 'Fill the tank up with gas',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('To-Do API Root');
@@ -25,7 +18,7 @@ app.get('/todos', function (req, res) {
 });
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
-    var todoID = parseInt(req.params.id);
+    var todoID = parseInt(req.params.id, 10);
     var matchedTodo;
     todos.forEach (function (todo) {
         if (todo.id === todoID) {
@@ -38,6 +31,15 @@ app.get('/todos/:id', function(req, res) {
         res.status(404).send('No matching todo found');
     }
 });
+
+// POST new todo../todos/
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    body.id = todoNextId++;
+    todos.push(body);
+    res.json(body);
+});
+
 
 app.listen(PORT, function () {
     console.log('Express Server listening on port ' + PORT + '!');
