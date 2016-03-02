4b22991bf5e5d6cc938c181f07bacf64dbf724c6
frankpl <frankpl@outlook.com>
2016-03-02 18:23:21 -0500

    Add PUT api to todo

--- server.js
+++ server.js
@@ -57,6 +57,34 @@ app.delete('/todos/:id', function (req, res) {
     }
 });
 
+// PUT method to update todos by id
+app.put('/todos/:id', function(req, res) {
+    var todoID = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoID});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        res.status(404).json({"Error": "No matching todo found"});
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if(body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description.trim();
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+});
+
+
 
 app.listen(PORT, function () {
     console.log('Express Server listening on port ' + PORT + '!');
