866225f992b7fc4cc90ada9d2f495f57dfbd0474
frankpl <frankpl@outlook.com>
2016-03-03 15:12:47 -0500

    Reference variables and debugging

---
+++ var-playground.js
@@ -0,0 +1,33 @@
+// var person = {
+//     name: "Andrew",
+//     age: 21
+// };
+
+// function updatePerson(obj) {
+//     // obj = {
+//     //     name: 'Andrew',
+//     //     age: 24
+//     // };
+//     obj.age = 24;
+// }
+
+var grades = [75, 85];
+
+function updateArray (gradesArr) {
+    gradesArr.push(95);
+    debugger;
+}
+
+// function changeArray (grades) {
+//     var grade = [95];
+//     grades = grades + grade;
+//     return grades;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+// updateArray(grades);
+updateArray(grades);
+console.log(grades);
