40c7cbb2c392a3f9ccb148b6bb3835951fd8f731
Lubin, Frank P <lubfr01@ca.com>
2016-03-21 18:41:23 -0400

    Convert GET todos to sequelize

--- server.js
+++ server.js
@@ -16,22 +16,26 @@ app.get('/', function (req, res) {
 
 // GET /todos
 app.get('/todos', function (req, res) {
-    var queryParams = req.query;
-    var filteredTodos = todos;
-
-    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-        filteredTodos = _.where(filteredTodos, {completed: true});
-    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-        filteredTodos = _.where(filteredTodos, {completed: false});
+    var query = req.query;
+    var where = {};
+    if (query.hasOwnProperty('completed') && query.completed === 'true') {
+        where.completed = true;
+    } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+        where.completed = false;
     }
-
-    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-        filteredTodos = _.filter(filteredTodos, function(todo) {
-            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-        });
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        var val = '%'+query.q+'%';
+        where.description = { $like: '%'+query.q+'%' };
     }
-
-    res.json(filteredTodos);
+    db.todo.findAll({where: where}).then(function(todo) {
+        if(todo.length > 0) {
+         res.json(todo);
+        } else {
+            res.status(404).json("Nothing returned");
+        }
+    }, function (e) {
+        res.status(500).send(e);
+    });
 });
 
 // GET /todos/:id
