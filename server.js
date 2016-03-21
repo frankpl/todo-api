396a9687929af96d47c1e957dc2078a0529f0654
Lubin, Frank P <lubfr01@ca.com>
2016-03-21 16:38:02 -0400

    Add GET by ID, add .gitconfig

--- server.js
+++ server.js
@@ -37,12 +37,16 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
     var todoID = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoID});
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send('No matching todo found');
-    }
+    db.todo.findById(todoID).then(function(todo) {
+        if (!!todo) {
+            res.json(todo.toJSON());
+        }
+        else {
+            res.status(404).json("No matching todo found");
+        }
+}, function(e) {
+    res.status(500).json(e);
+});
 });
 
 // POST new todo../todos/
@@ -54,17 +58,6 @@ app.post('/todos', function (req, res) {
     }).catch(function(e) {
         return res.status(400).json(e);
     });
-
-    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    //     return res.status(400).send();
-    // }
-
-    // body.description = body.description.trim();
-
-    // body.id = todoNextId++;
-
-    // todos.push(body);
-    // res.json(body);
 });
 
 // delete is the http method and the  route is /todos/:id
