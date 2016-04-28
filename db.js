94b90e2234f782e08fbadcf6806036274795447a
Lubin, Frank P <lubfr01@ca.com>
2016-04-28 14:15:47 -0400

    Refactor db connection after Heroku maintenance

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if (env === 'production') {
-    // sequelize = new Sequelize(process.env.DATABASE_URL, {
-    sequelize = new Sequelize('d7h3593njh5i8u', 'acibahdyzrrivf', 'lmrbvlvpqwEQHwHsRh7jud4w4F',{
-        host: 'ec2-107-22-248-209.compute-1.amazonaws.com',
-        dialect: 'postgres'
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+    // sequelize = new Sequelize('d7h3593njh5i8u', 'acibahdyzrrivf', 'lmrbvlvpqwEQHwHsRh7jud4w4F',{
+    //     host: 'ec2-107-22-248-209.compute-1.amazonaws.com',
+    //     dialect: 'postgres'
     });
 } else {
     var sequelize = new Sequelize(undefined, undefined, undefined, {
